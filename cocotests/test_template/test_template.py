d04cb5fa08d426c4caa0578fd631908b8e2ad2e3
Srinivas <ssunda03@calpoly.edu>
2025-05-30 00:30:00 -0700

    full test async fifo

--- cocotests/test_template/test_template.py
+++ cocotests/test_template/test_template.py
@@ -1,15 +1,121 @@
-
+import random
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import (
-    RisingEdge, FallingEdge,
-    Timer
-)
+from cocotb.triggers import RisingEdge, FallingEdge,Timer
+
+async def start_clocks(dut, w_per=7, r_per=13):
+    cocotb.start_soon(Clock(dut.i_wclk, w_per, units="ns").start())
+    cocotb.start_soon(Clock(dut.i_rclk, r_per, units="ns").start())
+
+async def wr_clk_edge_p(dut):
+    await RisingEdge(dut.i_wclk)
+
+async def rd_clk_edge_p(dut):
+    await RisingEdge(dut.i_rclk)
+
+async def clk_edge_p(dut):
+    await wr_clk_edge_p(dut)
+    await rd_clk_edge_p(dut)
+
+async def wr_clk_edge_n(dut):
+    await FallingEdge(dut.i_wclk)
+
+async def rd_clk_edge_n(dut):
+    await FallingEdge(dut.i_rclk)
+
+async def clk_edge_n(dut):
+    await wr_clk_edge_n(dut)
+    await rd_clk_edge_n(dut)
+    
+async def reset_dut(dut):
+    await start_clocks(dut)
+    
+    await wr_clk_edge_n(dut)
+    dut.i_wr.value = 0
+    dut.i_wrst_n.value = 0
+    dut.i_wdata.value = 0
+    
+    await rd_clk_edge_n(dut)
+    dut.i_rd.value = 0
+    dut.i_rrst_n.value = 0
+
+    await wr_clk_edge_n(dut)
+    dut.i_wrst_n.value = 1
+    
+    await rd_clk_edge_n(dut)
+    dut.i_rrst_n.value = 1
+    
+    await clk_edge_p(dut)
+    
+
+async def write(dut, val):
+    while dut.o_wfull.value:
+        await wr_clk_edge_p(dut)
+
+    await wr_clk_edge_n(dut)
+    dut.i_wdata.value = val
+    dut.i_wr.value = 1
+    
+    await wr_clk_edge_p(dut)
+    await wr_clk_edge_n(dut)
+
+    dut.i_wr.value = 0
+    await wr_clk_edge_p(dut)
+
+async def read(dut):
+    while dut.o_rempty.value:
+        await rd_clk_edge_p(dut)
+
+    await rd_clk_edge_n(dut)
+    dut.i_rd.value = 1
+    await rd_clk_edge_p(dut)
+    data = int(dut.o_rdata.value)
+    await rd_clk_edge_n(dut)
+    dut.i_rd.value = 0
+    await rd_clk_edge_p(dut)
+
+    return data
+
+async def check_value(dut, actual, expected, num):
+    assert actual == expected, f"{num} --> Actual: {actual}, Expected: {expected}"
+
 
 @cocotb.test()
-async def template_test(dut):
+async def test_fill(dut):
+    await reset_dut(dut)
+
+    width = 32
+    payload_list = []
+    while not dut.o_wfull.value:
+        payload = random.randint(0, 2 ** width - 1)
+        payload_list.append(payload)
+        await write(dut, payload)
+    print(len(payload_list))
     
-    cocotb.start_soon(Clock(dut.clk_r, 13, units='ns').start())
-    cocotb.start_soon(Clock(dut.clk_w, 7, units='ns').start())
+    i = 0
+    while not dut.o_rempty.value:
+        data = await read(dut)
+        await check_value(dut, data, payload_list[i], i)
+        i += 1
+    print(i)
+    await clk_edge_p(dut)
 
+@cocotb.test()
+async def test_alternate_fill(dut):
+    await reset_dut(dut)
 
+    width = 32
+    payload_list = []
+    i = 0
+    while not dut.o_wfull.value:
+        for _ in range(1 << i):
+            if not dut.o_wfull.value:
+                payload = random.randint(0, 2 ** width - 1)
+                payload_list.append(payload)
+                await write(dut, payload)
+            else:
+                break
+        data = await read(dut)
+        await check_value(dut, data, payload_list[i], i)
+        i += 1
+            
---
+++ rtl/AsyncFIFO.v
@@ -0,0 +1,115 @@
+module AsyncFIFO (
+    input wire i_wclk,
+    input wire i_rclk,
+
+    input wire i_wrst_n,
+    input wire i_rrst_n,
+
+    input wire i_wr,
+    input wire i_rd,
+
+    input wire [31:0] i_wdata,
+    output wire [31:0] o_rdata,
+
+    output wire o_wfull,
+    output wire o_rempty
+);
+
+    parameter	DATAW = 32;
+    parameter	ADRW =  8;
+
+    reg  [ADRW:0] wbin;
+    reg  [ADRW:0] wgray;
+    wire [ADRW:0] wbin_next;
+    wire [ADRW:0] wgray_next;
+    
+    reg  [ADRW:0] rbin;
+    reg  [ADRW:0] rgray;
+    wire [ADRW:0] rbin_next;
+    wire [ADRW:0] rgray_next;
+
+    reg  [31:0] mem [0:(1 << ADRW)-1];
+
+    reg [ADRW:0] rq1_wgray;
+    reg [ADRW:0] rq2_wgray;
+    reg [ADRW:0] wq1_rgray;
+    reg [ADRW:0] wq2_rgray;
+
+    wire we, re;
+    
+    initial begin
+        wbin = 0;
+        wgray = 0;
+        rbin = 0;
+        rgray = 0;
+
+        rq1_wgray = 0;
+        rq2_wgray = 0;
+        wq1_rgray = 0;
+        wq2_rgray = 0;
+
+        for (int i = 0; i < (1 << ADRW); i++) begin
+            mem[i] = 0;
+        end
+    end
+
+    
+    assign o_wfull = wgray[ADRW:ADRW-1] != wq2_rgray[ADRW:ADRW-1] && wgray[ADRW-2:0] == wq2_rgray[ADRW-2:0];
+    assign we = i_wr & !o_wfull;
+    assign wgray_next = wbin_next ^ (wbin_next >> 1);
+    assign wbin_next = wbin + {{(ADRW-1){1'b0}},we};
+    always_ff @( posedge i_wclk or negedge i_wrst_n ) begin
+        if (!i_wrst_n) begin
+            wbin <= 0;
+            wgray <= 0;
+        end
+        else begin
+            wbin <= wbin_next;
+            wgray <= wgray_next;
+        end
+
+        if (we) begin
+            mem[wbin[ADRW-1:0]] <= i_wdata;
+        end
+    end
+
+    // write domain --> read domain
+    always_ff @( posedge i_rclk or negedge i_rrst_n ) begin
+        if (!i_rrst_n) begin
+            { rq2_wgray, rq1_wgray } <= 0;
+        end
+        else begin
+            { rq2_wgray, rq1_wgray } <= { rq1_wgray, wgray };
+        end
+    end
+
+
+    assign o_rdata = mem[rbin[ADRW-1:0]];
+
+    assign	o_rempty = (rq2_wgray == rgray);
+    assign re = i_rd & !o_rempty;
+    assign rgray_next = rbin_next ^ (rbin_next >> 1);
+    assign rbin_next = rbin + {{(ADRW-1){1'b0}},re};
+    always_ff @( posedge i_rclk or negedge i_rrst_n ) begin
+        if (!i_rrst_n) begin
+            rbin <= 0;
+            rgray <= 0;
+        end
+        else begin
+            rbin <= rbin_next;
+            rgray <= rgray_next; 
+        end
+    end
+
+    // read domain --> write domain
+    always_ff @( posedge i_wclk or negedge i_wrst_n ) begin
+        if (!i_wrst_n) begin
+            { wq2_rgray, wq1_rgray } <= 0;
+        end
+        else begin
+            { wq2_rgray, wq1_rgray } <= { wq1_rgray, rgray };
+        end
+    end
+    
+
+endmodule
